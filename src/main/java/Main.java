c74701967290b013942f83e5fa6626016fbb2149
irinababunova <xleyla@gmail.com>
2023-10-29 14:59:05 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Fish fish = new Fish();
+        Duck duck = new Duck();
+        Animal kitten = new Cat();
+        cat.move();
+        kitten.move();
+        fish.move();
+        duck.move();
+    }
+}
